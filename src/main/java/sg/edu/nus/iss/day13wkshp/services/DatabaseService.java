70fbd777c7383f89d60bb18708074c2f9754663b
Neal Goh <nealgoh1996@gmail.com>
2022-07-20 15:09:36 +0800

    completed task 3

--- src/main/java/sg/edu/nus/iss/day13wkshp/services/DatabaseService.java
+++ src/main/java/sg/edu/nus/iss/day13wkshp/services/DatabaseService.java
@@ -1,7 +1,15 @@
 package sg.edu.nus.iss.day13wkshp.services;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Scanner;
 
 import org.springframework.stereotype.Service;
 
@@ -27,7 +35,13 @@ public class DatabaseService {
 
     public boolean save(final Contact contact) {
         File f = new File(this.dataDir, contact.getId());
-        try {
+        try (OutputStream out = new FileOutputStream(f)) {
+            PrintWriter pw = new PrintWriter(out);
+            pw.println(contact.getId());
+            pw.println(contact.getName());
+            pw.println(contact.getEmail());
+            pw.println(contact.getPhone());
+            pw.flush();
 
             return true;
         } catch (IOException ex) {
@@ -39,6 +53,31 @@ public class DatabaseService {
 	}
 
 	public Contact read(String fileId) {
-        return null;
+
+        try {
+           /*  File f = new File(this.dataDir, fileId);
+            Scanner myReader = new Scanner(f);
+            while (myReader.hasNextLine()) {
+                System.out.println(myReader.nextLine());
+            }
+            myReader.close(); */
+
+            Contact contact = new Contact();
+
+            Path filepPath = new File(this.dataDir, fileId).toPath();
+            Charset charset= Charset.forName("utf-8");
+            List<String> stringVal = Files.readAllLines(filepPath, charset);
+
+            contact.setName(stringVal.get(1));
+            contact.setEmail(stringVal.get(2));
+            contact.setPhone(stringVal.get(3));
+
+            return contact;
+
+        } catch (IOException ex) {
+            System.err.printf("Error: %s", ex.getMessage());
+            ex.printStackTrace();
+            return null;
+        }
 	}
 }
